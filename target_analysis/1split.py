8e063d3d1368739ad9c588263217c4f15cceca26
sking115422 <spencerd.king@gmail.com>
2022-01-06 18:48:31 -0500

    minor changes

--- target_analysis/1split.py
+++ target_analysis/1split.py
@@ -4,4 +4,4 @@ import pandas as pd
 DF = pd.read_csv(r'train.csv')
 for val in DF.Asset_ID.unique():
     print(val)
-    DF[DF.Asset_ID==val].sort_values(by=['timestamp'],ascending=True).to_csv(r'splitdata\Asset'+str(val)+'.csv',index=None)
+    DF[DF.Asset_ID==val].sort_values(by=['timestamp'],ascending=True).to_csv(r'splitdata/Asset'+str(val)+'.csv',index=None)
